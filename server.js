ae62672e2979db35e4466f17bc18fdf92d84a23f
LinkPlayer3c <linkplayer3c@gmail.com>
2025-12-23 11:17:57 +0500

    Okay

--- server.js
+++ server.js
@@ -27,7 +27,9 @@ app.use("/api/admin", adminRoutes);
 app.use("/api/contact", contactRoutes);
 
 //  START SERVER 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`)
-);
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () =>
+//   console.log(`Server running on http://localhost:${PORT}`)
+// );
+
+module.exports = {app}
