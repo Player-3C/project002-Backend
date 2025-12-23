0a2774d84dc2ee4a7d4bdbd745e62dd51cd07a43
LinkPlayer3c <linkplayer3c@gmail.com>
2025-12-23 14:29:51 +0500

    ok

--- server.js
+++ server.js
@@ -28,10 +28,10 @@ app.use("/api/admin", adminRoutes);
 app.use("/api/contact", contactRoutes);
 
 //  START SERVER
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
 
 // Export app if needed (for testing)
 export default app;
